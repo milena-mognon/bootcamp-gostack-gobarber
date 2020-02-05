d6827b87de6868ca8f7776cfd270207d6d06ce70
milena-mognon <milenamognon@gmail.com>
2020-02-05 19:29:53 -0300

    config to save file reference on database

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,11 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 // todos os models da aplicação
 // Models Loader
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
